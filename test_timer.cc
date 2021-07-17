07c3f62bd42fc78e18afb7b946e4dd625702164a
psshih <tommyannypeter@gapp.nthu.edu.tw>
2021-07-17 17:23:03 +0800

    main takes argument

--- test_timer.cc
+++ test_timer.cc
@@ -1,11 +1,20 @@
 #include "Timer.hh"
 #include <iostream>
 
-int main() {
-    system("clear");
-    int time = 3;
-    Timer timer = Timer(time);
-    timer.run();
+int main(int argc, char const *argv[]) {
+    if (argv[1] == "timer") {
+        system("clear");
+        int time = 3;
+        Timer timer = Timer(time);
+        timer.run();
+    }
+    else if (argv[1] == "board") {
+    }
+    else {
+        std::cout << "error" << std::endl;
+    }
 
+    system("pause");
     return 0;
 }
+
