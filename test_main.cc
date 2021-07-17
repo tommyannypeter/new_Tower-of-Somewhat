5bf85929319c7b709e21002f15ef996e6e7e4853
psshih <tommyannypeter@gapp.nthu.edu.tw>
2021-07-17 17:32:05 +0800

    add board exec

--- test_main.cc
+++ test_main.cc
@@ -1,4 +1,5 @@
 #include "Timer.hh"
+#include "Ball.hh"
 #include <iostream>
 
 int main(int argc, char const *argv[]) {
@@ -10,6 +11,23 @@ int main(int argc, char const *argv[]) {
         timer.run();
     }
     else if (exec_type == "board") {
+        Ball ball_1 = Ball(ColorName::Ball_grey);
+        ball_1.show();
+
+        Ball ball_2 = Ball(ColorName::Ball_cyan);
+        ball_2.picked_up();
+        ball_2.show();
+
+        Ball ball_3 = Ball(ColorName::Ball_green);
+        ball_3.picked_up();
+        ball_3.put_down();
+        ball_3.show();
+
+        Ball ball_4 = Ball(ColorName::Ball_red);
+        ball_4.show();
+        
+        Ball ball_5 = Ball(ColorName::Ball_yellow);
+        ball_5.show();
     }
     else {
         std::cout << "error" << std::endl;
