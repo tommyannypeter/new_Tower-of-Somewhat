ef86ef40041dfaba099a9f5b40a4b07b50c0c9fb
psshih <tommyannypeter@gapp.nthu.edu.tw>
2021-07-18 07:56:46 +0800

    add KeyboardInput

--- Utilities.cc
+++ Utilities.cc
@@ -1,5 +1,6 @@
 #include "Utilities.hh"
 #include <windows.h>
+#include <conio.h>
 
 void GoToCursorPosition(Coordinate coordinate) {
     COORD screen;
@@ -12,4 +13,26 @@ void GoToCursorPosition(Coordinate coordinate) {
 void ChangePrintColor(ColorName color_name) {
     int color_code = ColorTranslator::getInstance()->toColorCode(color_name);
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color_code);
+}
+
+KeyboardInput GetKeyboardInput() {
+    if (!kbhit()) {
+        if (getch() == 224) {
+            if (getch() == 72) {
+                return KeyboardInput::Up;
+            }
+            else if (getch() == 75) {
+                return KeyboardInput::Left;
+            }
+            else if (getch() == 77) {
+                return KeyboardInput::Right;
+            }
+            else if (getch() == 80) {
+                return KeyboardInput::Down;
+            }
+        }
+        else {
+            return KeyboardInput::Unclassified;
+        }
+    }
 }
--- Utilities.hh
+++ Utilities.hh
@@ -4,7 +4,17 @@
 #include "Coordinate.hh"
 #include "Color.hh"
 
+enum class KeyboardInput {
+    Up, 
+    Left, 
+    Right, 
+    Down, 
+
+    Unclassified, 
+};
+
 void GoToCursorPosition(Coordinate coordinate);
 void ChangePrintColor(ColorName color_name);
+KeyboardInput GetKeyboardInput();
 
 #endif
