376611f47a0db2c97efc6ad7f0d9c17ce126b625
psshih <tommyannypeter@gapp.nthu.edu.tw>
2021-07-18 08:07:01 +0800

    add board

---
+++ Board.cc
@@ -0,0 +1,7 @@
+#include "Board.hh"
+
+Board::Board() {
+}
+
+Board::~Board() {
+}
---
+++ Board.hh
@@ -0,0 +1,10 @@
+#ifndef BOARD_HH
+#define BOARD_HH
+
+class Board {
+public:
+    Board();
+    ~Board();
+}
+
+#endif
