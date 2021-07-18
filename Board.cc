d048f8f1abe1b23a0ede82bf6044796d9b5c4b4c
psshih <tommyannypeter@gapp.nthu.edu.tw>
2021-07-18 16:05:33 +0800

    add grid

--- Board.cc
+++ Board.cc
@@ -1,6 +1,12 @@
 #include "Board.hh"
+#include "Ball.hh"
 
-Board::Board(Coordinate coordinate) {
+Board::Board(Coordinate coordinate, int width, int height) 
+    : m_coordinate{coordinate}, m_width{width}, m_height{height} {
+    m_grid.reserve(width);
+    for (int column_num = 0; column_num < width; column_num ++) {
+        m_grid[column_num].reserve(height);
+    }
 }
 
 Board::~Board() {
--- Board.hh
+++ Board.hh
@@ -2,10 +2,17 @@
 #define BOARD_HH
 
 #include "Utilities.hh"
+#include <vector>
 
 class Board {
+private:
+    Coordinate m_coordinate;
+    int m_width;
+    int m_height;
+    std::vector<std::vector<Ball>> m_grid;
+
 public:
-    Board(Coordinate coordinate);
+    Board(Coordinate coordinate, int width, int height);
     ~Board();
 };
 
