c01498279e8a2161e73da55817ed58bdb3574be0
psshih <tommyannypeter@gapp.nthu.edu.tw>
2021-07-18 23:33:13 +0800

    fix cursor go outside of board

--- Cursor.cc
+++ Cursor.cc
@@ -12,25 +12,25 @@ void Cursor::setMaxXAndMaxY(int max_x, int max_y) {
 }
 
 void Cursor::moveUp() {
-    if (m_current_y != 0) {
+    if (m_current_y > 0) {
         m_current_y -= 1;
     }
 }
 
 void Cursor::moveLeft() {
-    if (m_current_x != 0) {
+    if (m_current_x > 0) {
         m_current_x -= 1;
     }
 }
 
 void Cursor::moveRight() {
-    if (m_current_x != m_max_x) {
+    if (m_current_x < m_max_x - 1) {
         m_current_x += 1;
     }
 }
 
 void Cursor::moveDown() {
-    if (m_current_y != m_max_y) {
+    if (m_current_y < m_max_y - 1) {
         m_current_y += 1;
     }
 }
